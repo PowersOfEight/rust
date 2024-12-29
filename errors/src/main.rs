f7512f52044468bf44e67d88ef97eaf5953a6bd0
PowersOfEight <jdjohnson8883@gmail.com>
2024-12-28 20:31:22 -0500

    A little example of results

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{ErrorKind, Write};
+use std::io::{self, ErrorKind, Read, Write};
 fn main() {
     let greeting_file_result = File::open("hello.txt");// File doesn't exist
 
@@ -20,4 +20,20 @@ fn main() {
         Ok(n) => println!("Wrote {n:05} bytes to file"),
         Err(e) => eprintln!("Could not write to file => \"{e:?}\""),
     }
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result { 
+		Ok(file) => file,
+		Err(e) => return Err(e),
+	};
+
+	let mut username = String::new();
+
+	match username_file.read_to_string(&mut username) {
+		Ok(_) => Ok(username),
+		Err(e) => Err(e),
+	}
 }

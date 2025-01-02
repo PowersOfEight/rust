cf87311cad6f814aa389478c988f752b2c28b2dc
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-02 15:37:01 -0500

    guess

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,10 +2,41 @@
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+pub fn add_two(a: usize) -> usize {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    // String::from("Hello!")
+    format!("Hello {name}!")
+}
+
 pub mod shapes;
+pub mod guess;
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        use guess::Guess;
+        Guess::new(200);
+    }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"    
+        );
+    }
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
 
     #[test]
     fn larger_can_hold_smaller() {
@@ -42,11 +73,6 @@ mod tests {
         assert!(!smaller.can_hold(&larger));
     }
 
-    // #[test]
-    // fn exploration() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
 
     // #[test]
     // fn another() {

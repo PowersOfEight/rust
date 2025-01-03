92e70181ca839cf0210cf91cee15c7b1c23d606d
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-03 15:20:33 -0500

    first test-driven implementation

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,8 +11,8 @@ fn main() {
         }
     );
 
-    println!("Searching for \"{}\"", config.query);
-    println!("In file \"{}\"", config.file_path);
+    // println!("Searching for \"{}\"", config.query);
+    // println!("In file \"{}\"", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");

fc0552ad07f278c90ca63da6e6b5ed972fcceba6
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-13 18:51:57 -0500

    Working with it

--- closure_shirt_example/src/main.rs
+++ closure_shirt_example/src/main.rs
@@ -56,4 +56,10 @@ fn main() {
         thread::sleep(Duration::from_secs(2));
         num
     };
+
+    fn add_one_v1 (x: u32) -> u32 { x + 1}
+
+    let add_one_v2 = |x: u32 | -> u32 { x + 1 };
+    let add_one_v4 = |x| x + 1 ;
+
 }

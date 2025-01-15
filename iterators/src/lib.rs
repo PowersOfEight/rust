32352a0f4a6f7993650a34a8dbf20287eae3ad40
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-15 18:53:26 -0500

    a little work with filters

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,3 +1,14 @@
+#[derive(PartialEq, Debug)]
+pub struct Shoe {
+    pub size: u32,
+    pub style: String,
+}
+
+pub fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -13,6 +24,25 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&2));
         assert_eq!(v1_iter.next(), Some(&3));
         assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 13, style: String::from("sandal") },
+            Shoe { size: 10, style: String::from("boot") },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe { size: 10, style: String::from("sneaker")},
+                Shoe { size: 10, style: String::from("boot")},
+            ]
+        )
     }
 
     #[test]

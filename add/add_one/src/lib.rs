f6d33ef78875a92032c88a7d14f5471ea806cf09
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-22 18:42:26 -0500

    working on it

---
+++ add/add_one/src/lib.rs
@@ -0,0 +1,28 @@
+//! # add_one
+//! A Library because it's a library.
+//! I'm just trying to be cool.
+//! 
+// #![deny(missing_docs)]
+/// Adds one to the number given
+/// and returns the result.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let input = 5;
+/// let result = add_one(input);
+/// assert_eq!(6, result);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_add_one() {
+        let result = add_one(2);
+        assert_eq!(result, 3);
+    }
+}

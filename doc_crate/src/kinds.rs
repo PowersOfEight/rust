70f0f79ab5e083f55151d9b89975a8855b512696
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-22 18:10:45 -0500

    Exporting libraries

---
+++ doc_crate/src/kinds.rs
@@ -0,0 +1,9 @@
+pub struct Color;
+
+pub struct PrimaryColor {
+    pub value: Color,
+}
+
+pub struct SecondaryColor {
+    pub value: Color,
+}
--- doc_crate/src/lib.rs
+++ doc_crate/src/lib.rs
@@ -1,3 +1,24 @@
+//! # doc_crate
+//! 
+//! `doc_crate` is a collection of utilities to demonstrate how to document a Rust crate.
+//! 
+mod kinds;
+
+pub use self::kinds::{PrimaryColor, SecondaryColor};
+
+
+/// Adds two numbers together.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let five = 5;
+/// let seven = 7;
+/// 
+/// let result = doc_crate::add(five, seven);
+/// 
+/// assert_eq!(12, result);
+/// ```
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }

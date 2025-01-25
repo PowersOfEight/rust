368135c7dcbeaf505f59789002c9880e95b12fa4
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-24 20:00:12 -0500

    added a listerator!

--- ref-count/src/main.rs
+++ ref-count/src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(4, Rc::clone(&a));
+    let c= Cons(3, Rc::clone(&a));
+
+    // Start at C
+    let mut d = &c;
+
+    while let Cons(i, next) = d {
+        println!("Current: {}", i);
+        d = next;
+    }
+    println!("End of list: {:?}", d);
 }

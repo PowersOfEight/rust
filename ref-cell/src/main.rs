cb28475f382266793c1acebccd6c8ec1b3788aa9
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-26 19:47:13 -0500

    some silliness

--- ref-cell/src/main.rs
+++ ref-cell/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 42;
+    let mut x = x;
+    x = 51;
+    println!("x: {}", x);
+    let mut y = &x;
+    println!("y: {}", y);
+    let z = 51;
+    y = &z;
+    println!("*y: {}", *y);
+    
+    let mut a = Box::new(y);
+
+    println!("*a: {}", *a);
 }

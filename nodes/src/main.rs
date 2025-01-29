910f5fa97cdbde46ca1e86d558ef5ffd563157db
PowersOfEight <jdjohnson8883@gmail.com>
2025-01-28 20:16:30 -0500

    15.6 weak references/pointers

--- nodes/src/main.rs
+++ nodes/src/main.rs
@@ -1,3 +1,12 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }

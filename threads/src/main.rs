1e4070bb3e57ddb4bf1bcf928523edc40953a95c
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-04 18:47:51 -0500

    Now with JoinHandle

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,4 +1,6 @@
+use std::fmt::Debug;
 use std::thread;
+use std::thread::JoinHandle;
 use std::time::Duration;
 use std::env::args;
 
@@ -6,12 +8,21 @@ fn main() {
 
     if let Some(name) = args().nth(0) {
         println!("Threading \"{name}\"");
+        let mut join_handles= vec![];
         for letter in name.chars() {
 
-            thread::spawn(move || {
+            let handle = thread::spawn(move || {
                 thread::sleep(Duration::from_millis(1));
                 println!("{letter}");
             });
+            join_handles.push(handle);
+        }
+
+        for handle in join_handles {
+            match handle.join() {
+                Ok(_) => println!("Thread finished"),
+                Err(_) => println!("Thread panicked"),
+            }
         }
     }
 

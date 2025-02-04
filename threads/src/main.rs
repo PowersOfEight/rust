75e0f2947cad78dd6f3150ce64f92c658df9fc21
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-04 18:39:27 -0500

    Added a little race condition

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,3 +1,29 @@
+use std::thread;
+use std::time::Duration;
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+
+    if let Some(name) = args().nth(0) {
+        println!("Threading \"{name}\"");
+        for letter in name.chars() {
+
+            thread::spawn(move || {
+                thread::sleep(Duration::from_millis(1));
+                println!("{letter}");
+            });
+        }
+    }
+
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
 }

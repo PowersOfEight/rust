d80545796b2ca9b76df9d787f003061b7a06494b
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-04 19:01:38 -0500

    Being wrong is the best learning experience sometimes

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -6,37 +6,47 @@ use std::env::args;
 
 fn main() {
 
-    if let Some(name) = args().nth(0) {
-        println!("Threading \"{name}\"");
-        let mut join_handles= vec![];
-        for letter in name.chars() {
-
-            let handle = thread::spawn(move || {
-                thread::sleep(Duration::from_millis(1));
-                println!("{letter}");
-            });
-            join_handles.push(handle);
-        }
-
-        for handle in join_handles {
-            match handle.join() {
-                Ok(_) => println!("Thread finished"),
-                Err(_) => println!("Thread panicked"),
-            }
-        }
-    }
-
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
+    let mut n = 1;
+    let t = thread::spawn(move || {
+        n = n + 1;
+        thread::spawn(move || {
+            n = n + 1;
+        }); 
     });
+    n = n + 1;
+    t.join().unwrap();
+    println!("{n}");
+    // if let Some(name) = args().nth(0) {
+    //     println!("Threading \"{name}\"");
+    //     let mut join_handles= vec![];
+    //     for letter in name.chars() {
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
+    //         let handle = thread::spawn(move || {
+    //             thread::sleep(Duration::from_millis(1));
+    //             println!("{letter}");
+    //         });
+    //         join_handles.push(handle);
+    //     }
 
-    handle.join().unwrap();
+    //     for handle in join_handles {
+    //         match handle.join() {
+    //             Ok(_) => println!("Thread finished"),
+    //             Err(_) => println!("Thread panicked"),
+    //         }
+    //     }
+    // }
+
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {i} from the spawned thread!");
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+
+    // handle.join().unwrap();
+    // for i in 1..5 {
+    //     println!("hi number {i} from the main thread!");
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
 }

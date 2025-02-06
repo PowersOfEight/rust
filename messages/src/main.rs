d7f98a00a20a0c28820af9c6c739c691e3d28cd3
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-05 19:32:03 -0500

    First example in 16.2

--- messages/src/main.rs
+++ messages/src/main.rs
@@ -1,3 +1,17 @@
+use std::sync::mpsc;
+// mpsc stands for multiple producer, single consumer
+use std::thread;
 fn main() {
-    println!("Hello, world!");
+    // (upstream, -> downstream)
+    let (producer, consumer) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi from the producer");
+        producer.send(val).unwrap();
+        println!("Value sent: {val}");
+    });
+
+    let received = consumer.recv().unwrap();// blocking call
+    // to use non-blocking call, use try_recv()
+    println!("Got: {received}");
 }

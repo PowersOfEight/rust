b7a392082386c7a0095e2a978f770bb605136220
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-05 19:42:02 -0500

    Second example

--- messages/src/main.rs
+++ messages/src/main.rs
@@ -1,17 +1,25 @@
 use std::sync::mpsc;
 // mpsc stands for multiple producer, single consumer
 use std::thread;
+use std::time::Duration;
 fn main() {
     // (upstream, -> downstream)
     let (producer, consumer) = mpsc::channel();
 
     thread::spawn(move || {
         let val = String::from("hi from the producer");
-        producer.send(val).unwrap();
-        println!("Value sent: {val}");
+        let val = val
+            .split_whitespace()
+            .for_each(|x| {
+                producer.send(x.to_string()).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            });
+        // producer.send(val).unwrap();
     });
 
-    let received = consumer.recv().unwrap();// blocking call
+    // let received = consumer.recv().unwrap();// blocking call
     // to use non-blocking call, use try_recv()
-    println!("Got: {received}");
+    for received in consumer {
+        println!("Got: {received}");
+    }
 }

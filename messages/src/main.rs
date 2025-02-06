90fff189042921e7da478024f502906704e09e2b
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-05 19:51:08 -0500

    That's a wrap for tonight.  Pick it up at 16.3 tomorrow

--- messages/src/main.rs
+++ messages/src/main.rs
@@ -6,15 +6,24 @@ fn main() {
     // (upstream, -> downstream)
     let (producer, consumer) = mpsc::channel();
 
+    let producer_clone = producer.clone();
     thread::spawn(move || {
-        let val = String::from("hi from the producer");
-        let val = val
+        String::from("hi from the producer")
+            .split_whitespace()
+            .for_each(|x| {
+                producer_clone.send(x.to_string()).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            });
+        // producer.send(val).unwrap();
+    });
+
+    thread::spawn(move || {
+        String::from("More messages from another producer")
             .split_whitespace()
             .for_each(|x| {
                 producer.send(x.to_string()).unwrap();
                 thread::sleep(Duration::from_secs(1));
             });
-        // producer.send(val).unwrap();
     });
 
     // let received = consumer.recv().unwrap();// blocking call

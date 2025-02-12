661a29d3cf88cf134e24d13ecdf294eec96788c0
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-11 19:55:34 -0500

    Enough examples for meow

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,3 +1,35 @@
+use std::future::Future;
+use trpl::Html;
+
+
+// Equivalent function shown below
+async fn page_title(url: &str) -> Option<String> {
+    // let response = trpl::get(url).await;
+    // let response_text = response.text().await;
+    let response_text = trpl::get(url).await.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(
+            |title_element| title_element.inner_html()
+        )
+}
+
+
+// Equivalent async function shown below
+fn page_title_equivalent(url: &str) -> impl Future<Output = Option<String>> {
+    async move {
+        Html::parse(
+            &trpl::get(url)
+                .await
+                .text()
+                .await
+        ).select_first("title")
+        .map(
+
+            |title_element| title_element.inner_html()
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
 }

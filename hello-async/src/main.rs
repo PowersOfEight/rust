e40d52d6f572a874ae3864cf804708b6e1674308
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-11 20:09:07 -0500

    listing 17-4

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,7 +1,6 @@
 use std::future::Future;
 use trpl::Html;
 
-
 // Equivalent function shown below
 async fn page_title(url: &str) -> Option<String> {
     // let response = trpl::get(url).await;
@@ -9,27 +8,25 @@ async fn page_title(url: &str) -> Option<String> {
     let response_text = trpl::get(url).await.text().await;
     Html::parse(&response_text)
         .select_first("title")
-        .map(
-            |title_element| title_element.inner_html()
-        )
+        .map(|title_element| title_element.inner_html())
 }
 
-
 // Equivalent async function shown below
-fn page_title_equivalent(url: &str) -> impl Future<Output = Option<String>> {
+fn page_title_equivalent(url: &str) -> impl Future<Output = Option<String>> + '_ {
     async move {
-        Html::parse(
-            &trpl::get(url)
-                .await
-                .text()
-                .await
-        ).select_first("title")
-        .map(
-
-            |title_element| title_element.inner_html()
-        )
+        Html::parse(&trpl::get(url).await.text().await)
+            .select_first("title")
+            .map(|title_element| title_element.inner_html())
     }
 }
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    trpl::run(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            None => println!("{url} had no title"),
+        }
+    })
 }

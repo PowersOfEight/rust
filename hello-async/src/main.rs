963dfc2a19a2075aba14d8065b9a8c96e8c13b26
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-12 20:00:08 -0500

    moving onto 17.2

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,32 +1,44 @@
 use std::future::Future;
-use trpl::Html;
+use trpl::{Html, Either};
 
 // Equivalent function shown below
-async fn page_title(url: &str) -> Option<String> {
+async fn page_title(url: &str) -> (&str, Option<String>) {
     // let response = trpl::get(url).await;
     // let response_text = response.text().await;
     let response_text = trpl::get(url).await.text().await;
-    Html::parse(&response_text)
+    let title = Html::parse(&response_text)
         .select_first("title")
-        .map(|title_element| title_element.inner_html())
+        .map(|title_element| title_element.inner_html());
+    (url, title)
 }
 
 // Equivalent async function shown below
-fn page_title_equivalent(url: &str) -> impl Future<Output = Option<String>> + '_ {
+fn page_title_equivalent(url: &str) -> impl Future<Output = (&str, Option<String>)> + '_ {
     async move {
-        Html::parse(&trpl::get(url).await.text().await)
+       let title = Html::parse(&trpl::get(url).await.text().await)
             .select_first("title")
-            .map(|title_element| title_element.inner_html())
+            .map(|title_element| title_element.inner_html());
+        (url, title)
     }
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            None => println!("{url} had no title"),
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title_equivalent(&args[2]);
+
+        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("\"{url}\" returned first");
+        match maybe_title {
+            Some(title ) => println!("The page title is \"{title}\""),
+            None => println!("The page has no title"),
         }
     })
 }
+

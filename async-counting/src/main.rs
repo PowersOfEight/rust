3600bbd07c61c096d24de146f0ad46f4d16f6519
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-13 19:21:30 -0500

    listing 17-7

--- async-counting/src/main.rs
+++ async-counting/src/main.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 fn main() {
     trpl::run(async {
 
-        trpl::spawn_task( async {
+        let handle =trpl::spawn_task( async {
             for i in 1..10 {
                 println!("hi number {i} from the first task!");
                 trpl::sleep(Duration::from_millis(500)).await;
@@ -12,7 +12,12 @@ fn main() {
 
         for i in 1..5 {
             println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(300)).await;
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        match handle.await {
+            Ok(()) => println!("task joined successfully"),
+            Err(_) => println!("task was aborted"),
         }
     });
 }

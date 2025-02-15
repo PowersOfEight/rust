53cb2bd2b6c30498b0eb50862db23f95a25fae8b
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-14 20:25:57 -0500

    Listing 17-11

--- async-messages/src/main.rs
+++ async-messages/src/main.rs
@@ -1,22 +1,27 @@
 use std::time::Duration;
 fn main() {
-    trpl::run( async {
+    trpl::run(async {
         let (tx, mut rx) = trpl::channel();
 
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("future"),
-        ];
+        let tx_fut = async {
+            let vals = vec![
+                String::from("hello"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        for val in vals {
-            tx.send(val).unwrap();
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
-        while let Some(val) = rx.recv().await {
-            println!("received \"{val}\"");
-        }
-    }
-    );
+        let rx_fut = async {
+            while let Some(val) = rx.recv().await {
+                println!("received \"{val}\"");
+            }
+        };
+        
+        trpl::join(tx_fut, rx_fut).await;
+    });
 }

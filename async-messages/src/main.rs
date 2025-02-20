d8bdf3d7059067b8e26747d243b93fe315d10126
PowersOfEight <jdjohnson8883@gmail.com>
2025-02-19 19:25:55 -0500

    listing 17-19

--- async-messages/src/main.rs
+++ async-messages/src/main.rs
@@ -1,5 +1,6 @@
-
-use std::time::Duration;
+use std::future::Future;
+use std::{time::Duration, vec};
+use std::pin::{pin, Pin};
 async fn print_letters() {
     let a = async {print!("A")};
     let b = async {print!("B")};
@@ -12,7 +13,7 @@ fn main() {
     trpl::run(async {
         let (tx, mut rx) = trpl::channel();
         let tx_clone = tx.clone();
-        let tx_clone_fut = async move {
+        let tx_clone_fut = pin!(async move {
             let vals = vec![
                 String::from("hello"),
                 String::from("from"),
@@ -23,15 +24,15 @@ fn main() {
                 tx.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        };
+        });
 
-        let rx_fut = async {
+        let rx_fut = pin!(async {
             while let Some(val) = rx.recv().await {
                 println!("received \"{val}\"");
             }
-        };
+        });
         
-        let tx_fut = async move {
+        let tx_fut = pin!(async move {
             let vals = vec![
                 String::from("more"),
                 String::from("messages"),
@@ -42,8 +43,15 @@ fn main() {
                 tx_clone.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(1500)).await;
             }
-        };
-        trpl::join3(tx_clone_fut, tx_fut, rx_fut).await;
+        });
+        // trpl::join!(tx_clone_fut, tx_fut, rx_fut); // works too
+        // trpl::join3(tx_clone_fut, tx_fut, rx_fut).await;
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![
+            tx_clone_fut,
+            tx_fut, 
+            rx_fut
+            ];
+        trpl::join_all(futures).await;
         print_letters().await;
     });
 

715f6755932f95c09999032cda840c4354a01c5f
PowersOfEight <jdjohnson8883@gmail.com>
2025-03-02 18:12:13 -0500

    second example with filters

--- async/streams/src/main.rs
+++ async/streams/src/main.rs
@@ -11,8 +11,25 @@ async fn stream_example_one() {
         println!("The value was: {value}");
     }
 }
+
+
+async fn stream_example_two() {
+    let stream = trpl::stream_from_iter(
+        (1..101).into_iter().map(|x| x * 2)
+    );
+
+    let mut filtered = 
+        stream.filter(
+            |val| val % 3 == 0 || val % 5 == 0
+        );
+
+    while let Some(x) = filtered.next().await {
+        println!("The value was: {x}");
+    }
+}
 fn main() {
     trpl::run(async {
         stream_example_one().await;
+        stream_example_two().await;
     });
 }

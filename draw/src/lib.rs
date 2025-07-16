60c9c3bbeae30605189b498909572493c19c6518
Dan Johnson <jdjohnson8883@gmail.com>
2025-07-15 21:25:02 -0400

    Listing 18-9: traits

--- draw/src/lib.rs
+++ draw/src/lib.rs
@@ -1,18 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod gui {
 
-pub trait Draw {
-    fn draw(&self);
+    pub trait Draw {
+        fn draw(&self);
+    }
+
+    pub struct Screen {
+        pub components: Vec<Box<dyn Draw>>,
+    }
+
+    impl Screen {
+        pub fn run(&self) {
+            self.components
+                .iter()
+                .for_each(|component| component.draw());
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Button {
+        pub width: u32,
+        pub height: u32,
+        pub label: String,
+    }
+
+    impl Draw for Button {
+        fn draw(&self) {
+            // do the drawing here using the button reference
+            println!("Draw the Button: {:?}", self);
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // use super::*;
 }
---
+++ draw/src/main.rs
@@ -0,0 +1,39 @@
+use draw::gui::Draw;
+
+#[derive(Debug)]
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("Draw the SelectBox: {:?}", self);
+    }
+}
+
+use draw::gui::{Button, Screen};
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
